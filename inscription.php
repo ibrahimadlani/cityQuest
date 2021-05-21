7ab6af4f97906a11a5ffea9f52aee89c34eced23
Azarios <azarios.development@gmail.com>
2021-05-21 21:30:38 +0200

    Corrections de bugs mineurs

--- inscription.php
+++ inscription.php
@@ -48,7 +48,7 @@ require_once("inc/views/head.inc.php");
     <hr>
 
     <label for="email" class="sr-only"><b>Adresse Email</b></label>
-    <input type="text" placeholder="Adresse Email" name="email" class="form-control top" required>
+    <input type="email" placeholder="Adresse Email" name="email" class="form-control top" required>
 
     <label for="psw" class="sr-only"><b>Mot de passe</b></label>
     <input type="password" placeholder="Mot de passe" name="mdp" class="form-control center" required>
@@ -64,13 +64,12 @@ require_once("inc/views/head.inc.php");
 
     <div class="checkbox mb-3">
         <label>
-          <input type="checkbox" value="remember-me"> Se souvenir de moi
+            <input type="checkbox" value="remember-me"> Se souvenir de moi
         </label>
-      </div>
-      <button class="btn btn-lg btn-danger btn-block rounded-pill w-100" name="submit" type="submit">S'inscrire</button>
-      <a href="connexion.php" class="text-danger"><small>Déjà un compte? Connectez-vous ici.</small></a>
-      <p class="my-5 text-center text-secondary"><small>© City<b class="text-danger">Quest</b> 2020 - Tous Droits Réservés</small>
-  </div>
+    </div>
+    <button class="btn btn-lg btn-danger btn-block rounded-pill w-100" name="submit" type="submit">S'inscrire</button>
+    <a href="connexion.php" class="text-danger"><small>Déjà un compte ? Connectez-vous ici.</small></a>
+    <p class="my-5 text-center text-secondary"><small>© City<b class="text-danger">Quest</b> 2020 - Tous Droits Réservés</small></p>
 </form>
 
 
