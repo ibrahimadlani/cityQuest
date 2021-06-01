b5170804328d5a52bdaba6bc77b4339b6bde4617
Azarios <azarios.development@gmail.com>
2021-06-01 14:59:00 +0200

    Correction mineures

--- inc/views/caseEvenement.inc.php
+++ inc/views/caseEvenement.inc.php
@@ -4,14 +4,21 @@ require_once('../../config/db.php');
 require_once('../../lib/pdo_db.php');
 ?>
 
-<?php foreach ($_POST["jsonFile"] as $e) {?>
+<?php
+$count = 0;
+foreach ($_POST["jsonFile"] as $e) {
+    $count++;
+    ?>
     <div class="row" data-bs-toggle="collapse">
         <div class="my-3 p-4  border rounded" id="<?php echo $e["id"]; ?>">
             <div class="col-12">
                 <h1 class="fw-bold text-center"><?php echo $e['nom']; ?></h1>
-                <p class="mb-0  text-center"><span class="lead "><?php echo $e['description']; ?></span><br><small class="text-danger"><?php echo 'du ' . date_format(new DateTime($e['debut']), 'd/m/Y') . ' au ' . date_format(new DateTime($e['fin']), 'd/m/Y'); ?></small></p>
+                <p class="mb-0  text-center"><span class="lead "><?php echo $e['description']; ?></span><br><small
+                            class="text-danger"><?php echo 'du ' . date_format(new DateTime($e['debut']), 'd/m/Y') . ' au ' . date_format(new DateTime($e['fin']), 'd/m/Y'); ?></small>
+                </p>
             </div>
         </div>
     </div>
+    <?php } if ($count > 0) { ?>
+    <hr>
 <?php } ?>
-<hr>
